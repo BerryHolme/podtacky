392ba07851669593bec83cb720d2c75cae5db225
Matouš Suchý <matous.suchy33@gmail.com>
2024-01-24 21:17:57 +0100

    funkční javascript a načítací stránka!

--- podtackySorter.py
+++ podtackySorter.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, render_template_string, send_from_directory, session, redirect, url_for
+from flask import render_template 
 from flask_session import Session 
 import os
 from rembg import remove
@@ -15,6 +16,11 @@ from concurrent.futures import ProcessPoolExecutor
 from concurrent.futures import ThreadPoolExecutor
 from werkzeug.datastructures import FileStorage
 import asyncio
+from flask import jsonify
+from threading import Lock
+
+processing_status = {"picture1": "Nahrávání...", "picture2": "Nahrávání..."}
+status_lock = Lock()
 
 
 UPLOAD_FOLDER = 'Library'
@@ -31,306 +37,7 @@ Session(app)
 
 os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 
-HTML = '''
-<!doctype html>
-<html>
-<head>
-    <title>Nahrání podtácků</title>
-        <style>
-        body {
-            font-family: Arial, sans-serif;
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            height: 100vh;
-            margin: 0;
-            font-size: 50px;
-        }
-        .container {
-            background-color: #f5f5f5;
-            padding: 20px;
-            border-radius: 10px;
-            box-shadow: 0 2px 4px rgba(0, 0, 0, 0.2);
-            width: 90%; /* Menší šířka pro PC */
-            
-            font-size: 50px;
-        }
-        input[type=file], input[type=submit] {
-            margin-bottom: 10px;
-            width: 100%;
-            padding: 10px 20px; /* Standardní velikost paddingu */
-            font-size: 50px;
-        }
-        input[type=submit] {
-            background-color: red;
-            color: white;
-            border: none;
-            border-radius: 5px;
-            cursor: pointer;
-            font-size: 50px;
-        }
-        input[type=submit]:hover {
-            background-color: darkred;
-        }
 
-        /* Media query pro malé obrazovky (např. mobily) */
-        @media (max-width: 600px) {
-            body {
-                font-size: 20px; /* Větší písmo pro mobily */
-            }
-            .container {
-                width: 80%; /* Větší šířka pro mobily */
-                max-width: none;
-            }
-            input[type=file], input[type=submit] {
-                padding: 15px 30px; /* Větší tlačítka pro mobily */
-            }
-        }
-    </style>
-</head>
-<body>
-<div class="container">
-    <h2>Nahrajte dva obrázky podtácku z obou stran</h2>
-    {% if message %}
-    <p>{{ message }}</p>
-    {% endif %}
-    <form method="post" enctype="multipart/form-data">
-        <input type="file" name="file1" required><br><br>
-        <input type="file" name="file2" required><br><br>
-        <input type="submit" value="Nahrát">
-    </form>
-</div>
-</body>
-</html>
-'''
-
-RESULTS_HTML = '''
-<!doctype html>
-<html>
-<head>
-    <title>Výsledky porovnání podtácků</title>
-    <style>
-        body {
-            font-family: Arial, sans-serif;
-            margin: 0;
-            font-size: 50px; /* Zvětšení písma */
-            display: flex;
-            flex-direction: column;
-            align-items: center;
-        }
-        @media (max-width: 400px) {
-            body {
-                font-size: 16px;
-            }
-        }
-        .header, .uploaded-images, .results-container {
-            width: 90%; /* Zvětšení šířky */
-            max-width: 1000px; /* Zvětšení maximální šířky */
-            text-align: center;
-            margin-bottom: 20px;
-        }
-        .container {
-            background-color: #f5f5f5;
-            padding: 20px;
-            border-radius: 10px;
-            box-shadow: 0 2px 4px rgba(0, 0, 0, 0.2);
-            margin: 10px;
-            flex: 1; 
-        }
-        .results-container {
-            display: flex;
-            justify-content: center;
-            flex-wrap: wrap;
-        }
-        .images img {
-            max-width: 100%;
-            max-height: 250px; /* Zvětšení velikosti obrázků */
-            margin: 0 10px;
-        }
-
-        .buttons{
-            flex-direction: row;
-            align-items: center;
-        }
-
-        button {
-            background-color: red;
-            color: white;
-            border: none;
-            padding: 12px 24px; /* Zvětšení tlačítka */
-            border-radius: 5px;
-            cursor: pointer;
-            margin-bottom: 20px;
-            font-size: 50px; /* Zvětšení textu tlačítka */
-        }
-        button:hover {
-            background-color: darkred;
-        }
-        @media (max-width: 300px) {
-            /* Kontejnery pod sebou na menších obrazovkách */
-            .results-container {
-                flex-direction: row;
-                align-items: center;
-            }
-        }
-    </style>
-</head>
-<body>
-    <div class="header">
-        <h2>Výsledky porovnání podtácků</h2>
-    </div>
-<div class="buttons">
-    <a href="/"><button>Uložit podtácek</button></a>
-    <a href="/delete"><button>Vymazat podtácek</button></a>
-</div>
-    <div class="uploaded-images">
-        <h2>Nově nahraný podtácek</h2>
-        {% if uploaded_images %}
-            {% for image_base64 in uploaded_images %}
-                <img src="data:image/png;base64,{{ image_base64 }}" alt="Nahraný obrázek" style="max-width: 400px; margin: 10px;">
-            {% endfor %}
-        {% endif %}
-    </div>
-    <div class="results-container">
-    <div class="container">
-        <h3>Podobné podtácky 1:</h3>
-        {% if similarities %}
-        <ul>
-            {% for similarity, path, image_base64 in similarities %}
-            <li>{{ similarity|round(2) }}% shoda - {{ path }}<br>
-            <img src="data:image/png;base64,{{ image_base64 }}" alt="Obrázek" style="max-width: 200px;"><br></li>
-            {% endfor %}
-        </ul>
-        {% else %}
-        <p>Žádné podobné podtácky nebyly nalezeny.</p>
-        {% endif %}
-    </div>
-    <div class="container">
-        <h3>Podobné podtácky 2:</h3>
-        {% if similarities2 %}
-        <ul>
-            {% for similarity, path, image_base64 in similarities2 %}
-            <li>{{ similarity|round(2) }}% shoda - {{ path }}<br>
-            <img src="data:image/png;base64,{{ image_base64 }}" alt="Obrázek" style="max-width: 200px;"><br></li>
-            {% endfor %}
-        </ul>
-        {% else %}
-        <p>Žádné podobné podtácky nebyly nalezeny.</p>
-        {% endif %}
-    </div>
-</div>
-
-</body>
-</html>
-
-
-'''
-PROCESS_HTML = '''
-<!doctype html>
-<html>
-<head>
-    <title>Probíhá proces obrázků</title>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
-    <style>
-        body {
-            font-family: Arial, sans-serif;
-            margin: 0;
-            font-size: 50px; /* Zvětšení písma */
-            display: flex;
-            flex-direction: column;
-            align-items: center;
-        }
-        @media (max-width: 400px) {
-            body {
-                font-size: 16px;
-            }
-        }
-        .header, .uploaded-images, .results-container {
-            width: 90%; /* Zvětšení šířky */
-            max-width: 1000px; /* Zvětšení maximální šířky */
-            text-align: center;
-            margin-bottom: 20px;
-        }
-        .container {
-            background-color: #f5f5f5;
-            padding: 20px;
-            border-radius: 10px;
-            box-shadow: 0 2px 4px rgba(0, 0, 0, 0.2);
-            margin: 10px;
-            flex: 1; 
-        }
-        .results-container {
-            display: flex;
-            justify-content: center;
-            flex-wrap: wrap;
-        }
-        .images img {
-            max-width: 100%;
-            max-height: 250px; /* Zvětšení velikosti obrázků */
-            margin: 0 10px;
-        }
-
-        .buttons{
-            flex-direction: row;
-            align-items: center;
-        }
-
-        button {
-            background-color: red;
-            color: white;
-            border: none;
-            padding: 12px 24px; /* Zvětšení tlačítka */
-            border-radius: 5px;
-            cursor: pointer;
-            margin-bottom: 20px;
-            font-size: 50px; /* Zvětšení textu tlačítka */
-        }
-        button:hover {
-            background-color: darkred;
-        }
-        @media (max-width: 300px) {
-            /* Kontejnery pod sebou na menších obrazovkách */
-            .results-container {
-                flex-direction: row;
-                align-items: center;
-            }
-        }
-    </style>
-
-    <script>
-    // Funkce pro provedení AJAX požadavku na procesování obrázků
-    function startImageProcessing() {
-        $.ajax({
-            url: "/process", // URL pro procesování
-            type: "POST",
-            success: function (data) {
-                // Po úspěšném zpracování přesměrování na stránku s výsledky
-                window.location.href = "/results";
-            },
-            error: function () {
-                alert("Chyba při zpracování obrázků.");
-            }
-        });
-    }
-
-    // Po načtení stránky spustit procesování automaticky
-    $(document).ready(function () {
-        startImageProcessing();
-    });
-</script>
-
-</head>
-<body>
-    <div class="header">
-        <h2>Výsledky porovnání podtácků</h2>
-    </div>
-Probíhá konverze obrázků
-
-</body>
-</html>
-
-
-'''
 
 def image_to_base64(image_path):
     with open(image_path, "rb") as image_file:
@@ -359,14 +66,19 @@ def upload_file():
         return redirect(url_for('process_page'))
 
     
-    return render_template_string(HTML)
+    return render_template('index.html')
 
 @app.route('/process_page', methods=['GET'])
 def process_page():
-    return render_template_string(PROCESS_HTML)
+    session['picture1'] = ''
+    session['picture2'] = ''
+    return render_template('results.html')
 
 @app.route('/process', methods=['POST'])
 def process():
+    with status_lock:
+        processing_status["picture1"] = "Nahrávám..."
+        processing_status["picture1"] = "Nahrávám..."
     similarities = []
     similarities2 = []
     next_folder_name = get_next_folder_name()
@@ -381,12 +93,19 @@ def process():
 
     def process_image(file, index):
         print("Konvertuju...")
+        global processing_status
+        with status_lock:
+            processing_status[f"picture{index}"] = "Konvertuju..."
+
         image = Image.open(file.stream)
         image = image.convert('RGB')  # Odstranění alfa kanálu pro PNG
         file_path = os.path.join(next_folder, f'{index}.png')
         image.save(file_path, 'PNG', quality=95, optimize=True, exif='')
 
         print("Odstraňuju pozadí...")
+        with status_lock:
+            processing_status[f"picture{index}"] = "Odstraňuju pozadí"
+
         # Odstranění pozadí pomocí rembg
         with open(file_path, 'rb') as img_file:
             input_image = img_file.read()
@@ -399,6 +118,7 @@ def process():
     uploaded_files_paths = []
     uploaded_files_base64 = []
 
+
     with ThreadPoolExecutor(max_workers=2) as executor:
         future1 = executor.submit(process_image, file1, 1)
         future2 = executor.submit(process_image, file2, 2)
@@ -410,11 +130,12 @@ def process():
         uploaded_files_base64.append(result2[1])
 
     # Paralelní hledání podobných obrázků
-    with ProcessPoolExecutor(max_workers=2) as executor:
+    with ThreadPoolExecutor(max_workers=2) as executor:
         future1 = executor.submit(find_similar_images, uploaded_files_paths[0])
         future2 = executor.submit(find_similar_images, uploaded_files_paths[1])
         similarities = future1.result()
         similarities2 = future2.result()
+
 
     print("Dokončeno")
 
@@ -422,21 +143,30 @@ def process():
     if similarities:
         message += ' a nalezeny podobné obrázky'
 
+        
+    with status_lock:
+        processing_status["picture1"] = "Posílám..."
+        processing_status["picture1"] = "Posílám..."
+
     session['uploaded_images'] = uploaded_files_base64
     session['similarities'] = similarities
     session['similarities2'] = similarities2
-    return "success"  # Návratová hodnota pro AJAX požadavek
+    
+    data = {
+        'uploaded_images': session.get('uploaded_images', []),
+        'similarities': session.get('similarities', []),
+        'similarities2': session.get('similarities2', [])
+    }
+
+    return jsonify(data)  # Return JSON response with data
 
 
+@app.route('/getStatus', methods=['GET'])
+def getStatus():
+    with status_lock:
+        status_copy = processing_status.copy()
+    return jsonify(status_copy)
 
-
-@app.route('/results')
-def results():
-    similarities = session.get('similarities', [])
-    similarities2 = session.get('similarities2', [])
-    uploaded_images = session.get('uploaded_images', [])
-
-    return render_template_string(RESULTS_HTML, similarities=similarities, similarities2=similarities2, uploaded_images=uploaded_images)
 
 
 def calculate_similarity(image1, image2):
@@ -457,16 +187,25 @@ def calculate_similarity(image1, image2):
     return similarity[0][0]
 
 def find_similar_images(new_image_path):
+    global processing_status
+    image_index = os.path.basename(new_image_path).replace('.png', '')
+
     similarities = []
     new_image_folder = os.path.dirname(new_image_path)
-
-    # Vytvoření seznamu všech souborů PNG pro zpracování
     all_files = [os.path.join(root, file) for root, dirs, files in os.walk(UPLOAD_FOLDER) for file in files if file.endswith('.png')]
+
+    total_files = len(all_files)
+    processed_files = 0
     
-    # Použití tqdm pro progress bar
-    for file_path in tqdm(all_files, desc="Hledání podobností", unit="soubor"):
+    for file_path in all_files:
         if file_path == new_image_path or os.path.dirname(file_path) == new_image_folder:
             continue
+
+        processed_files += 1
+        progress = (processed_files / total_files) * 100
+        with status_lock:
+            processing_status[f"picture{image_index}"] = f"Zpracovávám {processed_files}/{total_files} souborů ({progress:.2f}%)"
+
         new_image_data = cv2.imread(new_image_path)
         existing_image_data = cv2.imread(file_path)
         similarity = calculate_similarity(new_image_data, existing_image_data)
@@ -475,7 +214,11 @@ def find_similar_images(new_image_path):
             image_base64 = image_to_base64(file_path)
             similarities.append((similarity*100, file_path, image_base64))
 
+    with status_lock:
+        processing_status[f"picture{image_index}"] = "Dokončeno"
+
     return similarities
+
 
 
 
